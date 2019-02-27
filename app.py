01251dbd8a99baba98301d57288ddd364a86c3f1
Manpreet <32339352+left-over-dinner@users.noreply.github.com>
2019-02-27 15:30:48 -0500

    #4 removed unwanted git merge info on file

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 from flask import Blueprint
 from flask_restful import Api
 from resources.Hello import Hello
-<<<<<<< HEAD
-=======
 from resources.registerRoute import UserRegister
 from resources.identifyRoute import Login, Logout
 from resources.patientRoute import PatientMake, PatientUpdate, PatientCheck, PatientCancel
@@ -11,7 +9,6 @@ from resources.doctorRoute import DoctorUpdate
 #
 # from resources.Category import CategoryResource
 # from resources.Comment import CommentResource
->>>>>>> de3f8ea627aff3ef7c5cb745267afaa2e810bfce
 from resources.Patient import PatientResource
 from resources.Doctor import DoctorResource
 from resources.Nurse import NurseResource
