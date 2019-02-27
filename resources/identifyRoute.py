9420a33a6b1fbb897ebc3cec5d832f94af6bdec2
Anthony Iatropoulos <35300538+aiatro@users.noreply.github.com>
2019-02-27 13:32:39 -0500

    Revert "#4 template routes and React installation completed"

--- resources/identifyRoute.py
+++
@@ -1,9 +0,0 @@
-from flask_restful import Resource
-
-class Login(Resource):
-    def get(self):
-        return {"message": "from login"}
-
-class Logout(Resource):
-    def get(self):
-        return {"message": "from logout"}
