61479f4707d8696c2c5efc0bbff0ad32cbfd4f0b
Manpreet <many_nangla@hotmail.com>
2019-03-01 14:35:14 -0500

    #23 #22  API with Facade Pattern, & fixed DB attr.
    work done with team member Eglen @eglenceco

---
+++ resources/LoginResource.py
@@ -0,0 +1,20 @@
+from flask import request
+from flask_restful import Resource
+from Model import db
+from Classes.DatabaseFacade import DatabaseFacade
+
+dbFacade = DatabaseFacade.getInstance(db)
+
+
+
+
+class LoginResource(Resource):
+
+    def get(self):
+        json_data = request.get_json(force=True)
+        if not json_data:
+            return {'status':'failure','message': 'No input data provided'}, 400
+        account = dbFacade.login(json_data['type'], json_data['email'],json_data['password'])
+        if 'email' not in account:
+            return {'status': 'failure','message':'Invalid login'}, 400
+        return {'status': 'success', 'data': account}, 200
--- resources/NurseResource.py
+++ resources/NurseResource.py
@@ -1,17 +1,18 @@
 from flask import request
 from flask_restful import Resource
 from Model import db, Nurse, NurseSchema
+from Classes.DatabaseFacade import DatabaseFacade
 #
 nurses_schema = NurseSchema(many=True)
 nurse_schema = NurseSchema()
 
+dbFacade = DatabaseFacade.getInstance(db)
 
 # this file has to be correctly implemented
 class NurseResource(Resource):
 
     def get(self):
-        nurses = Nurse.query.all()
-        nurses = nurses_schema.dump(nurses).data
+        nurses = dbFacade.getNurses()
         return {'status': 'success', 'data': nurses}, 200
 
     ...
@@ -20,23 +21,9 @@ class NurseResource(Resource):
         json_data = request.get_json(force=True)
         if not json_data:
             return {'message': 'No input data provided'}, 400
-        # Validate and deserialize input
-        data, errors = nurse_schema.load(json_data)
-        if errors:
-            return errors, 422
-        nurse = Nurse.query.filter_by(access_id=data['access_id']).first()
-        if nurse:
-            return {'message': 'nurse already exists'}, 400
-        nurse = Nurse(
-            access_id=json_data['access_id'],
-            password=json_data['password'],
-        )
-
-        db.session.add(nurse)
-        db.session.commit()
-
-        result = nurse_schema.dump(nurse).data
-
+        result = dbFacade.registerNurse(json_data)
+        if 'error' in result:
+            return {"status": 'failure', 'message': result['error']}, 400
         return {"status": 'success', 'data': result}, 201
 
     ...
@@ -45,20 +32,10 @@ class NurseResource(Resource):
         json_data = request.get_json(force=True)
         if not json_data:
             return {'message': 'No input data provided'}, 400
-        # Validate and deserialize input
-        data, errors = nurse_schema.load(json_data)
-        if errors:
-            return errors, 422
-        nurse = Nurse.query.filter_by(access_id=data['access_id']).first()
-        if not nurse:
-            return {'message': 'Category does not exist'}, 400
-        nurse.access_id = json_data['access_id'],
-        nurse.password = json_data['password'],
-        db.session.commit()
-
-        result = nurse_schema.dump(nurse).data
-
-        return {"status": 'success', 'data': result}, 204
+        result = dbFacade.updateNurse(json_data)
+        if 'error' in result:
+            return {"status": 'failure', 'message': result['error']}, 400
+        return {"status": 'success', 'data': result}, 200
 
     ...
 
@@ -66,13 +43,8 @@ class NurseResource(Resource):
         json_data = request.get_json(force=True)
         if not json_data:
             return {'message': 'No input data provided'}, 400
-        # Validate and deserialize input
-        data, errors = nurse_schema.load(json_data)
-        if errors:
-            return errors, 422
-        nurse = Nurse.query.filter_by(access_id=data['access_id']).delete()
-        db.session.commit()
+        result = dbFacade.removeNurse(json_data)
+        if 'error' in result:
+            return {"status": 'failure', 'message': result['error']}, 400
+        return {"status": 'success', 'data': result}, 200
 
-        result = nurse_schema.dump(nurse).data
-
-        return {"status": 'success', 'data': result}, 204
