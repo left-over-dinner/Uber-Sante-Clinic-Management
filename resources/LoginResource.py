fb3f6646a00c2ad9bce1fde2e36f827cb11eaae4
Erdem <erdemkepenek1@gmail.com>
2019-03-11 10:57:39 -0400

    Merge branch 'front-end' of C:\Users\Erdem\PycharmProjects\soen344 with conflicts.

--- resources/LoginResource.py
+++ resources/LoginResource.py
@@ -11,10 +11,12 @@ dbFacade = DatabaseFacade.getInstance(db)
 class LoginResource(Resource):
 
     def post(self):
-        json_data = request.get_json(force=True)
+        print("hellllo")
+        json_data = request.get_json();
+        print(json_data)
         if not json_data:
-            return {'status':'failure','message': 'No input data provided'}, 400
+            return {'status':'failure','message': 'No input data provided'}, 401
         account = dbFacade.login(json_data['type'], json_data['email'],json_data['password'])
         if 'email' not in account:
-            return {'status': 'failure','message':'Invalid login'}, 400
+            return {'status': 'failure','message':'Invalid login'}, 401
         return {'status': 'success', 'data': account}, 200
